0775d92d354b280ea49b0abfacd101612c8cf31c
MilanBhanuka <vgmbhanuka@gmail.com>
2024-06-02 01:46:16 +0530

    oragization dashboard

---
+++ lib/sidebar.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          const UserAccountsDrawerHeader(
+            accountName: Text("Green Planet"),
+            accountEmail: Text("testing@gmail.com"),
+            currentAccountPicture: CircleAvatar(
+              backgroundColor: Colors.white,
+              backgroundImage: AssetImage('assets/logo.png'),
+              child: Text("G"),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Home'),
+            onTap: () {
+              // Handle the tap event
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.apps),
+            title: Text('Applications'),
+            onTap: () {
+              // Handle the tap event
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.assignment),
+            title: Text('Programs'),
+            onTap: () {
+              // Handle the tap event
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.people),
+            title: Text('Volunteers'),
+            onTap: () {
+              // Handle the tap event
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.notifications),
+            title: Text('Notifications'),
+            onTap: () {
+              // Handle the tap event
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.chat),
+            title: Text('Chats'),
+            onTap: () {
+              // Handle the tap event
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
