6ac5ed2111aa825ef82014ab963e6a33331eb0d0
isuru-liyanage <isuruliyanageofficial@gmail.com>
2024-06-01 22:04:41 +0530

    project commit

---
+++ lib/login.dart
@@ -0,0 +1,287 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:toastification/toastification.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+
+class _LoginPageState extends State<LoginPage> {
+
+  @override
+  void initState()  {
+    super.initState();
+
+  }
+
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+
+  bool _isObscure = true; // Track whether the password is obscured or not
+
+  Future<void> _signup() async {
+    var headers = {'Content-Type': 'application/x-www-form-urlencoded'};
+    var request = http.Request('POST', Uri.parse('http://madhack.codingblinders.com/auth/signin'));
+    request.bodyFields = {
+      'email': _emailController.text,
+      'password': _passwordController.text,
+
+    };
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      final responseData = await response.stream.bytesToString();
+      final userDataJson = json.decode(responseData);
+
+      // Extracting data from the response
+      String userId = userDataJson['data']['id'];
+      String userEmail = userDataJson['data']['email'];
+      String userRole = userDataJson['data']['role'];
+      String userToken = userDataJson['data']['token'];
+
+
+
+      // Store user data in SharedPreferences
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setString('userId', userId);
+      await prefs.setString('userEmail', userEmail);
+      await prefs.setString('userRole', userRole);
+      await prefs.setString('userToken', userToken);
+      print(responseData);
+
+      toastification.show(
+        context: context,
+        title: Text('Successfully Signed In!',style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600),),
+        autoCloseDuration: const Duration(seconds: 3),
+        type: ToastificationType.success,
+        style: ToastificationStyle.flatColored,
+        alignment: Alignment.topCenter,
+        direction: TextDirection.ltr,
+        showProgressBar: false,
+        closeButtonShowType: CloseButtonShowType.none,
+        closeOnClick: false,
+        pauseOnHover: true,
+        dragToClose: true,
+        applyBlurEffect: true,
+      );
+      if(userRole == 'jobApplicant'){
+        // Navigator.of(context).pushReplacement(
+        //   MaterialPageRoute(
+        //     // builder: (context) => HomeScreen(),
+        //   ),
+        // );
+
+      }
+      else if(userRole=='employer'){
+        // Navigator.of(context).pushReplacement(
+        //   MaterialPageRoute(
+        //     builder: (context) => JobPosting(),
+        //   ),
+        // );
+
+      }
+
+
+    } else {
+      final responseData = await response.stream.bytesToString();
+      final userDataJson = json.decode(responseData);
+
+      toastification.show(
+        context: context,
+        title: Text(userDataJson['message'],style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600),),
+        autoCloseDuration: const Duration(seconds: 3),
+        type: ToastificationType.error,
+        style: ToastificationStyle.fillColored,
+        alignment: Alignment.topCenter,
+        direction: TextDirection.ltr,
+        showProgressBar: false,
+        closeButtonShowType: CloseButtonShowType.none,
+        closeOnClick: false,
+        pauseOnHover: true,
+        dragToClose: true,
+        applyBlurEffect: true,
+      );
+
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text('Welcome Back!',
+              style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 100.0,),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+
+
+                Text(
+                  'Email',
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+                _buildInputField(_emailController, 'user@mail.com'),
+              ],
+            ),
+            SizedBox(height: 20), // Add gap between input fields and labels
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Password',
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8.0),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        spreadRadius: 1,
+                        blurRadius: 30,
+                        offset: Offset(0, 0), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: TextFormField(
+                    controller: _passwordController,
+                    obscureText: _isObscure,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: '********',
+                      hintStyle: TextStyle(
+                        color: Color.fromARGB(255, 163, 163, 163),
+                      ),
+                      contentPadding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0), // Adjust padding
+
+
+
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _isObscure ? Icons.visibility_off : Icons.visibility,
+                          color: Colors.grey,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _isObscure = !_isObscure;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 80), // Add gap between input fields and labels
+
+            SizedBox(height: 30), // Add larger gap after last input field
+            ElevatedButton(
+              onPressed: _signup,
+              style: ElevatedButton.styleFrom(
+                primary: Color.fromARGB(223, 217, 217, 217), // Set button color to gray
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10), // Set border radius
+                ),
+                minimumSize: Size(190, 50), // Set button size
+              ),
+              child: Text('Sign In',style: TextStyle(
+                color: Colors.black,
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold
+
+              ),),
+            ),
+            SizedBox(height: 20.0,),
+            Text('or Sign in with',style: TextStyle(
+              color: Colors.grey
+            ),),
+            Row(
+
+              mainAxisAlignment: MainAxisAlignment.center, // Adjust the spacing between buttons
+
+              children: [
+                IconButton(onPressed: (){}, icon: Icon(
+                  Icons.mail,
+                  color: Colors.grey,),
+                  iconSize: 24,),
+                IconButton(onPressed: (){}, icon: Icon(
+                  Icons.facebook,
+                  color: Colors.grey,),
+                  iconSize: 24,),
+                IconButton(onPressed: (){}, icon: Icon(
+                  Icons.apple,
+                  color: Colors.grey,),
+                  iconSize: 24,)
+              ],
+            ),
+            SizedBox(height: 30.0,),
+            GestureDetector(
+              onTap: () {
+                // Navigator.of(context).pushReplacement(
+                //     MaterialPageRoute(
+                //       builder: (context) => SignupPage(),
+                //     ),
+                //    );
+              },
+              child: Text(
+                'Don\'t have an Account? Signup',
+                              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildInputField(TextEditingController controller, String label) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8.0),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            spreadRadius: 1,
+            blurRadius: 30,
+            offset: Offset(0, 0), // changes position of shadow
+          ),
+        ],
+      ),
+      child: TextFormField(
+        controller: controller,
+
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          hintText: label,
+          hintStyle: TextStyle(
+            color: Color.fromARGB(255, 163, 163, 163),
+          ),
+          contentPadding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0), // Adjust padding
+
+
+
+        ),
+      ),
+    );
+  }
+}
+
---
+++ lib/signup.dart
@@ -0,0 +1,374 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:toastification/toastification.dart';
+
+import 'login.dart';
+
+class SignupPage extends StatefulWidget {
+  @override
+  _SignupPageState createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  String _selectedRole = 'jobApplicant';
+
+  bool _isObscure = true; // Track whether the password is obscured or not
+  bool _isObscure1 = true;
+  Future<void> _signup() async {
+
+    // SharedPreferences prefs = await SharedPreferences.getInstance();
+    // Retrieve user data from SharedPreferences
+    // String userId = prefs.getString('userId') ?? '';
+    // String userEmail = prefs.getString('userEmail') ?? '';
+    // String userRole = prefs.getString('userRole') ?? '';
+    // String userToken = prefs.getString('userToken') ?? '';
+    //
+    // // Now you can use the retrieved data as needed
+    // print('User ID: $userId');
+    // print('User Email: $userEmail');
+    // print('User Role: $userRole');
+    // print('User Token: $userToken');
+
+    var headers = {'Content-Type': 'application/x-www-form-urlencoded'};
+    var request = http.Request('POST', Uri.parse('http://madhack.codingblinders.com/auth/signup'));
+    request.bodyFields = {
+      'email': _emailController.text,
+      'password': _passwordController.text,
+      'confirm_password': _confirmPasswordController.text,
+      'name': _nameController.text,
+      'role': _selectedRole,
+    };
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      final responseData = await response.stream.bytesToString();
+      final userDataJson = json.decode(responseData);
+
+      // Extracting data from the response
+      String userId = userDataJson['data']['id'];
+      String userEmail = userDataJson['data']['email'];
+      String userRole = userDataJson['data']['role'];
+      String userToken = userDataJson['data']['token'];
+
+      // Store user data in SharedPreferences
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setString('userId', userId);
+      await prefs.setString('userEmail', userEmail);
+      await prefs.setString('userRole', userRole);
+      await prefs.setString('userToken', userToken);
+      print(responseData);
+      if(userRole == 'jobApplicant'){
+        Navigator.pushNamed(context, '/home');
+      }else{
+        Navigator.pushNamed(context, '/emp');
+      }
+
+    } else {
+      toastification.show(
+        context: context,
+        title: Text(response.reasonPhrase.toString()),
+        autoCloseDuration: const Duration(seconds: 2),
+        type: ToastificationType.error,
+        style: ToastificationStyle.fillColored,
+        alignment: Alignment.bottomCenter,
+        direction: TextDirection.ltr,
+        showProgressBar: false,
+        closeButtonShowType: CloseButtonShowType.none,
+        closeOnClick: false,
+        pauseOnHover: false,
+        dragToClose: true,
+        applyBlurEffect: true,
+      );
+      // print(response.reasonPhrase);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text('Create an Account',
+              style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 30.0,),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+
+
+                Text(
+                  'Email',
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+                _buildInputField(_emailController, 'user@mail.com'),
+              ],
+            ),
+            SizedBox(height: 10), // Add gap between input fields and labels
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Password',
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8.0),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        spreadRadius: 1,
+                        blurRadius: 30,
+                        offset: Offset(0, 0), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: TextFormField(
+                    controller: _passwordController,
+                    obscureText: _isObscure1,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: '********',
+                      hintStyle: TextStyle(
+                        color: Color.fromARGB(255, 163, 163, 163),
+                      ),
+                      contentPadding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0), // Adjust padding
+
+
+
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _isObscure1 ? Icons.visibility_off : Icons.visibility,
+                          color: Colors.grey,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _isObscure1 = !_isObscure1;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10), // Add gap between input fields and labels
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Confirm Password',
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8.0),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        spreadRadius: 1,
+                        blurRadius: 30,
+                        offset: Offset(0, 0), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: TextFormField(
+                    controller: _confirmPasswordController,
+                    obscureText: _isObscure,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: '********',
+                      hintStyle: TextStyle(
+                        color: Color.fromARGB(255, 163, 163, 163),
+                      ),
+                      contentPadding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0), // Adjust padding
+
+
+
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _isObscure ? Icons.visibility_off : Icons.visibility,
+                          color: Colors.grey,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _isObscure = !_isObscure;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10), // Add gap between input fields and labels
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Name',
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+                _buildInputField(_nameController, 'Saman Kumara'),
+              ],
+            ),
+            SizedBox(height: 10), // Add gap between input fields and labels
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Role',
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width - 40,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8.0),
+
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        spreadRadius: 1,
+                        blurRadius: 30,
+                        offset: Offset(0, 0), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: DropdownButtonFormField<String>(
+                    value: _selectedRole,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
+
+
+                    ),
+
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _selectedRole = newValue!;
+                      });
+                    },
+                    isExpanded: true,
+                    items: <String>['jobApplicant', 'employer']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 30), // Add larger gap after last input field
+            ElevatedButton(
+              onPressed: _signup,
+              style: ElevatedButton.styleFrom(
+                primary: Color.fromARGB(223, 217, 217, 217), // Set button color to gray
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10), // Set border radius
+                ),
+                minimumSize: Size(190, 50), // Set button size
+              ),
+              child: Text('Sign Up',style: TextStyle(
+                color: Colors.black,
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold
+
+              ),),
+            ),
+            SizedBox(height: 6.0,),
+            Text('or Sign up with',style: TextStyle(
+              color: Colors.grey
+            ),),
+            Row(
+
+              mainAxisAlignment: MainAxisAlignment.center, // Adjust the spacing between buttons
+
+              children: [
+                IconButton(onPressed: (){}, icon: Icon(
+                  Icons.mail,
+                  color: Colors.grey,),
+                  iconSize: 24,),
+                IconButton(onPressed: (){}, icon: Icon(
+                  Icons.facebook,
+                  color: Colors.grey,),
+                  iconSize: 24,),
+                IconButton(onPressed: (){}, icon: Icon(
+                  Icons.apple,
+                  color: Colors.grey,),
+                  iconSize: 24,)
+              ],
+            ),
+            SizedBox(height: 30.0,),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (context) => LoginPage(),
+                  ),
+                );
+              },
+              child: Text(
+                'Already a Member? Sign In',
+                              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildInputField(TextEditingController controller, String label) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8.0),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            spreadRadius: 1,
+            blurRadius: 30,
+            offset: Offset(0, 0), // changes position of shadow
+          ),
+        ],
+      ),
+      child: TextFormField(
+        controller: controller,
+
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          hintText: label,
+          hintStyle: TextStyle(
+            color: Color.fromARGB(255, 163, 163, 163),
+          ),
+          contentPadding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0), // Adjust padding
+
+
+
+        ),
+      ),
+    );
+  }
+}
+
